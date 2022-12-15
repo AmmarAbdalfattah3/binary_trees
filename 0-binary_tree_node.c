35e68eb32b04ac71fd4f0bd0a2b0b09f32e6d15a
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-12-15 15:38:17 -0800

    Creating nodes

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,8 +14,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	{
 		return (NULL);
 	}
-	node->parent = parent;
 	node->n = value;
+	node->parent = parent;
 	node->right = NULL;
 	node->left = NULL;
 	return (node);
