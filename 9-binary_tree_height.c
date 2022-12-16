33a72d18f3c99349223c4ebee4856fc73b81df83
AmmarAbdalFattah3 <ammmarabdalfattah251@gmail.com>
2022-12-16 09:06:23 -0800

    binary tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -5,7 +5,7 @@
  * @tree: is a pointer to the root node of the tree to measure the height.
  * Return: the height of a binary tree or (0) if tree is NULL.
  */
-size_t binary_tree_height(const binary_tree_t *tree);
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
